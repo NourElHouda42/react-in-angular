a4a12aeff4bf65273164362915da1bb0c803b3cc
NourElHouda42 <97087208+NourElHouda42@users.noreply.github.com>
2023-03-17 18:05:34 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject } from '@angular/core';
+import { NgReact } from './ng-react';
+import { App } from './reactComponent';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ``
 })
 export class AppComponent {
   title = 'react-in-angular';
+  private ngReact = inject(NgReact);
+  private root = this.ngReact.createRoot(inject(ElementRef).nativeElement);
+
+  ngOnInit() {
+    this.ngReact.render(this.root, App)
+  }
+
+  ngOnDestroy() {
+    this.root.unmount();
+  }
 }
